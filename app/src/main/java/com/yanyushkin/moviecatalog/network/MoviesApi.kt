c05ff5668c4a054488edfc886a1d87bcb56eeb61
theKirill <k.a.yanushkin@gmail.com>
2019-07-11 21:34:57 +0300

    fixed search request

--- app/src/main/java/com/yanyushkin/moviecatalog/network/MoviesApi.kt
+++ app/src/main/java/com/yanyushkin/moviecatalog/network/MoviesApi.kt
@@ -13,6 +13,6 @@ interface MoviesApi {
     @GET("discover/movie?api_key=" + BuildConfig.ApiKey + "&language=ru&region=ru&sort_by=release_date.desc&year=2019")
     fun getAllMovies(@Query("page") page: Int): Call<MoviesResponse>
 
-    @GET("search/movie?api_key=" + BuildConfig.ApiKey + "6ccd72a2a8fc239b13f209408fc31c33&language=ru&region=ru")
+    @GET("search/movie?api_key=" + BuildConfig.ApiKey + "&language=ru&region=ru")
     fun getNecessaryMovies(@Query("query") query: String): Call<MoviesResponse>
 }
